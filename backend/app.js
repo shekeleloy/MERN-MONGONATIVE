b1c8e66e4f54392bae11d3fd34fe5d805e726de0
nicole <Maria.Medina@idessit.com>
2024-03-11 16:59:43 +0800

    app

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const path = require ('path')
 const dotenv = require ('dotenv')
+const MongoClient = require('mongodb').MongoClient;
 
 dotenv.config({
     path: './.env',
 });
+
+const PORT = process.env.PORT || 5050;
+
+// Connection to MongoDB
+const uri = process.env.MONGO_URI;
+
+MongoClient.connect(url)
+.then (client => {
+    console.log("connectend to MongoDB");
+
+    app.listen(PORT, () => console.log("Server is listening no port"))
+})
